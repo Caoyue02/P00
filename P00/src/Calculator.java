561558de7a6cdea86a9c56ffbeae8ab5d56afe99
22008600 <22008600@10.172.144.217>
2023-05-25 10:07:06 +0800

    first push to P00 repository in GitHbu

---
+++ P00/src/Calculator.java
@@ -0,0 +1,33 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: 22008600
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Thursday 25-05-2023 09:28
+ */
+
+/**
+ * @author 22008600
+ *
+ */
+public class Calculator {
+	public int add(int a, int b ) {
+		return a + b;
+	}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		
+		return a*b;
+	}
+	
+	public int devide(int a, int b) {
+		
+		return a/b;
+	}
+}
